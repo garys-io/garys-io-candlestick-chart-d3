25c5db31578cc30bde66f660a73cff3bcf9082aa
gary <gsb.gurpreet@gmail.com>
2020-12-07 14:27:41 -0500

    Add extra timedelta options

--- src/dataTransformer.js
+++ src/dataTransformer.js
@@ -11,7 +11,7 @@ const DAY = 4
 const parsed10secData = data.map(({ time, volume, low, high, open, close }) => {
     const [hour, minute, second] = time.split(':')
     return {
-        time: new Date(YEAR, MONTH, DAY, hour, minute, second + 5),
+        time: new Date(YEAR, MONTH, DAY, hour, minute, second),
         volume: Number(volume),
         low: Number(low),
         high: Number(high),
@@ -38,20 +38,20 @@ const calcMetrics = list => ({
     close: calcClose(list),
 })
 
+// combine two 10 seconds to make 20 second data
 const parsed20secData = []
 for (let i = 0; i < parsed10secData.length + 2; i += 2) {
     if (!parsed10secData[i]) break
-    const t = parsed10secData[i].time
-    const time = new Date(YEAR, MONTH, DAY, t.getHours(), t.getMinutes(), t.getSeconds() + 5)
     const list = [parsed10secData[i], parsed10secData[i + 1]]
     if (!list.every(x => x)) break
 
     parsed20secData.push({
-        time,
+        time: parsed10secData[i].time,
         ...calcMetrics(list),
     })
 }
 
+// combine three 10 seconds to make 30 second data
 const parsed30secData = []
 for (let i = 0; i < parsed10secData.length + 3; i += 3) {
     if (!parsed10secData[i + 1]) break
@@ -59,51 +59,78 @@ for (let i = 0; i < parsed10secData.length + 3; i += 3) {
     if (!list.every(x => x)) break
 
     parsed30secData.push({
-        time: parsed10secData[i + 1].time,
+        time: parsed10secData[i].time,
         ...calcMetrics(list),
     })
 }
 
+// combine two 30 seconds to make 1 minute data
 const parsed1minData = []
-for (let i = 0; i < parsed10secData.length + 6; i += 6) {
-    if (!parsed10secData[i + 2]) break
-    const list = [
-        parsed10secData[i],
-        parsed10secData[i + 1],
-        parsed10secData[i + 2],
-        parsed10secData[i + 3],
-        parsed10secData[i + 4],
-        parsed10secData[i + 5],
-    ]
+for (let i = 0; i < parsed30secData.length + 2; i += 2) {
+    if (!parsed30secData[i + 2]) break
+    const list = [parsed30secData[i], parsed30secData[i + 1]]
     if (!list.every(x => x)) break
 
     parsed1minData.push({
-        time: parsed10secData[i + 2].time,
+        time: parsed30secData[i].time,
         ...calcMetrics(list),
     })
 }
 
+// combine two 1 minutes to make 2 minute data
 const parsed2minData = []
-for (let i = 0; i < parsed10secData.length + 12; i += 12) {
-    if (!parsed10secData[i + 5]) break
-    const list = [
-        parsed10secData[i],
-        parsed10secData[i + 1],
-        parsed10secData[i + 2],
-        parsed10secData[i + 3],
-        parsed10secData[i + 4],
-        parsed10secData[i + 5],
-        parsed10secData[i + 6],
-        parsed10secData[i + 7],
-        parsed10secData[i + 8],
-        parsed10secData[i + 9],
-        parsed10secData[i + 10],
-        parsed10secData[i + 11],
-    ]
+for (let i = 0; i < parsed1minData.length + 2; i += 2) {
+    if (!parsed1minData[i + 1]) break
+    const list = [parsed1minData[i], parsed1minData[i + 1]]
     if (!list.every(x => x)) break
 
     parsed2minData.push({
-        time: parsed10secData[i + 5].time,
+        time: parsed1minData[i].time,
+        ...calcMetrics(list),
+    })
+}
+
+// combine three 1 minutes to make 3 minute data
+const parsed3minData = []
+for (let i = 0; i < parsed1minData.length + 3; i += 3) {
+    if (!parsed1minData[i + 1]) break
+    const list = [parsed1minData[i], parsed1minData[i + 1], parsed1minData[i + 2]]
+    if (!list.every(x => x)) break
+
+    parsed3minData.push({
+        time: parsed1minData[i].time,
+        ...calcMetrics(list),
+    })
+}
+
+// combine five 1 minutes to make 5 minute data
+const parsed5minData = []
+for (let i = 0; i < parsed1minData.length + 5; i += 5) {
+    if (!parsed1minData[i + 2]) break
+    const list = [
+        parsed1minData[i],
+        parsed1minData[i + 1],
+        parsed1minData[i + 2],
+        parsed1minData[i + 3],
+        parsed1minData[i + 4],
+    ]
+    if (!list.every(x => x)) break
+
+    parsed5minData.push({
+        time: parsed1minData[i].time,
+        ...calcMetrics(list),
+    })
+}
+
+// combine two 5 minutes to make 10 minute data
+const parsed10minData = []
+for (let i = 0; i < parsed5minData.length + 2; i += 2) {
+    if (!parsed5minData[i + 1]) break
+    const list = [parsed5minData[i], parsed5minData[i + 1]]
+    if (!list.every(x => x)) break
+
+    parsed10minData.push({
+        time: parsed5minData[i].time,
         ...calcMetrics(list),
     })
 }
@@ -114,6 +141,9 @@ const timedelta = {
     '30 second': parsed30secData,
     '1 minute': parsed1minData,
     '2 minute': parsed2minData,
+    '3 minute': parsed3minData,
+    '5 minute': parsed5minData,
+    '10 minute': parsed10minData,
 }
 
 export { YEAR, MONTH, DAY, data as rawData, timedelta }
