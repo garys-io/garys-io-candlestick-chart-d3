7bd89860ca4290270fb3c1aa1d9ffcf06e48f6a1
gary <gsb.gurpreet@gmail.com>
2020-12-07 13:15:18 -0500

    Add option to select from and to times i.e zooming in the graph

--- src/App.js
+++ src/App.js
@@ -4,36 +4,53 @@ import { timedelta, YEAR, MONTH, DAY } from './dataTransformer'
 import { chartRenderer } from './chartRenderer'
 
 const defaultInterval = '1 minute'
+const defaultFromTime = new Date(YEAR, MONTH, DAY, 9, 30)
+const defaultToTime = new Date(YEAR, MONTH, DAY, 16, 0)
+const dateOptions = [
+    new Date(YEAR, MONTH, DAY, 9, 30),
+    new Date(YEAR, MONTH, DAY, 10, 0),
+    new Date(YEAR, MONTH, DAY, 10, 30),
+    new Date(YEAR, MONTH, DAY, 11, 0),
+    new Date(YEAR, MONTH, DAY, 11, 30),
+    new Date(YEAR, MONTH, DAY, 12, 0),
+    new Date(YEAR, MONTH, DAY, 12, 30),
+    new Date(YEAR, MONTH, DAY, 13, 0),
+    new Date(YEAR, MONTH, DAY, 13, 30),
+    new Date(YEAR, MONTH, DAY, 14, 0),
+    new Date(YEAR, MONTH, DAY, 14, 30),
+    new Date(YEAR, MONTH, DAY, 15, 0),
+    new Date(YEAR, MONTH, DAY, 15, 30),
+    new Date(YEAR, MONTH, DAY, 16, 0),
+]
 const width = 1920
 const height = 1080
 const margin = 50
 
 function App() {
     const svgRef = useRef()
-
+    // [, new Date(YEAR, MONTH, DAY, 16, 0)]
     const [interval, setInterval] = useState(defaultInterval)
-    const data = timedelta[interval]
-    const candleWidth = (0.42 * (width - 2 * margin)) / data.length
+    const [fromTime, setFromTime] = useState(defaultFromTime)
+    const [toTime, setToTime] = useState(defaultToTime)
+    const data = timedelta[interval].filter(d => d.time > fromTime && d.time < toTime)
 
     useEffect(() => {
-        console.log('called')
         chartRenderer({
             svgEle: svgRef.current,
-            YEAR,
-            MONTH,
-            DAY,
             width,
             height,
             margin,
-            candleWidth,
             data,
-            interval,
+            fromTime,
+            toTime
         })
-    }, [data, candleWidth])
+    }, [data])
 
     return (
         <>
             <svg ref={svgRef} width={width} height={height}>
+                <g id="x-axis"></g>
+                <g id="y-axis"></g>
                 <g id="graph"></g>
             </svg>
             <div>
@@ -46,6 +63,30 @@ function App() {
                     {Object.keys(timedelta).map(key => (
                         <option value={key} key={key}>
                             {key}
+                        </option>
+                    ))}
+                </select>
+                <label htmlFor="form-time">Choose from time:</label>
+                <select
+                    id="form-time"
+                    onChange={e => setFromTime(new Date(e.target.value))}
+                    defaultValue={defaultFromTime}
+                >
+                    {dateOptions.map(date => (
+                        <option value={date.toISOString()} key={date.toISOString()}>
+                            {date.toLocaleTimeString()}
+                        </option>
+                    ))}
+                </select>
+                <label htmlFor="to-time">Choose to time:</label>
+                <select
+                    id="to-time"
+                    onChange={e => setToTime(new Date(e.target.value))}
+                    defaultValue={defaultToTime}
+                >
+                    {dateOptions.map(date => (
+                        <option value={date.toISOString()} key={date.toISOString()}>
+                            {date.toLocaleTimeString()}
                         </option>
                     ))}
                 </select>
--- src/chartRenderer.js
+++ src/chartRenderer.js
@@ -1,12 +1,14 @@
 import { select, scaleTime, scaleLinear, min, max, axisBottom, axisLeft } from 'd3'
 
 const chartRenderer = props => {
-    const { svgEle, YEAR, MONTH, DAY, width, height, margin, candleWidth, data, interval } = props
+    const { svgEle, width, height, margin, data, fromTime, toTime } = props
+
+    const candleWidth = (0.42 * (width - 2 * margin)) / data.length
 
     const svg = select(svgEle).attr('viewBox', [0, 0, width, height])
 
     const x = scaleTime()
-        .domain([new Date(YEAR, MONTH, DAY, 9, 30), new Date(YEAR, MONTH, DAY, 16, 0)])
+        .domain([fromTime, toTime])
         .range([0, width - 2 * margin])
 
     const y = scaleLinear()
@@ -16,13 +18,14 @@ const chartRenderer = props => {
     const xAxis = axisBottom(x)
     const yAxis = axisLeft(y)
 
-    svg.append('g')
+    svg.select('#x-axis')
         .call(xAxis)
         .attr('transform', `translate(${margin}, ${height - margin})`)
 
-    svg.append('g').call(yAxis).attr('transform', `translate(${margin}, ${margin})`)
+    svg.select('#y-axis').call(yAxis).attr('transform', `translate(${margin}, ${margin})`)
 
-    svg.select('#graph').selectAll('line')
+    svg.select('#graph')
+        .selectAll('line')
         .data(data)
         .join('line')
         .attr('y1', d => y(d.low))
@@ -30,7 +33,8 @@ const chartRenderer = props => {
         .attr('stroke', 'black')
         .attr('transform', d => `translate(${x(d.time) + margin}, ${margin})`)
 
-    svg.select('#graph').selectAll('rect')
+    svg.select('#graph')
+        .selectAll('rect')
         .data(data)
         .join('rect')
         .attr('height', d => Math.abs(y(d.open) - y(d.close)))
